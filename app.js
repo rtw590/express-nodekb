3ead600fca3bc46af67288e6a0bc1d822521322c
Rob Waller <r.t.waller@gmail.com>
2018-02-13 13:29:23 -0600

    Finished through part 4. Connected to db and displaying it

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const path = require('path');
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://user1:password1@ds113826.mlab.com:13826/nodekb';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Test connection
+
+db.once('open', function(){
+    console.log('Connected to mongoDB');
+});
+
+
 // Initialize the app
 const app = express();
+
+// Bring in models
+let Article = require('./models/article');
 
 // Load view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -10,30 +28,16 @@ app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', function(req, res){
-    let articles = [
-        {
-            id: 1,
-            title: 'Article 1',
-            author: 'Rob',
-            body: 'This is article 1'
-        },
-        {
-            id: 2,
-            title: 'Article 2',
-            author: 'Daniel',
-            body: 'This is article 2'
-        },
-        {
-            id: 3,
-            title: 'Article 3',
-            author: 'Bobby',
-            body: 'This is article 3'
+    Article.find({}, function(err, articles){
+        // index is the name of the view ie index.pug
+        if(err){
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: 'Hello doggg',
+                articles: articles
+            });
         }
-    ];
-    // index is the name of the view ie index.pug
-    res.render('index', {
-        title: 'Hello doggg',
-        articles: articles
     });
 });
 
