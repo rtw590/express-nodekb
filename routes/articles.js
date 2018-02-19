e9b2e452c019caadc63aa50085b400680b987810
Rob Waller <r.t.waller@gmail.com>
2018-02-19 17:45:21 -0600

    Finished Part 11. Users can only edit and delete their own posts

--- routes/articles.js
+++ routes/articles.js
@@ -1,107 +1,126 @@
 const express = require('express');
 const router = express.Router();
 
-// Bring in Article model
+// Article Model
 let Article = require('../models/article');
-
-// Load Edit Form
-// :id is a placeholder and could be anything
-router.get('/edit/:id', function(req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('edit_article', {
-            title: 'Edit Article',
-            article: article
-        });
-    });
-});
+// User Model
+let User = require('../models/user');
 
 // Add Route
-router.get('/add', function(req, res){
-    // add_article is the name of the view ie add.pug
-    res.render('add_article', {
-        title: 'Add Articles'
-    });
+router.get('/add', ensureAuthenticated, function(req, res){
+  res.render('add_article', {
+    title:'Add Article'
+  });
 });
 
-// Add Submit POST route
-// Catches submitted items from add_article
-
+// Add Submit POST Route
 router.post('/add', function(req, res){
-    // Setting rules/validation for submitting form
-    req.checkBody('title', 'Title is required').notEmpty();
-    req.checkBody('author', 'Author is required').notEmpty();
-    req.checkBody('body', 'Body is required').notEmpty();
+  req.checkBody('title','Title is required').notEmpty();
+  //req.checkBody('author','Author is required').notEmpty();
+  req.checkBody('body','Body is required').notEmpty();
 
-    // Get Errors
+  // Get Errors
+  let errors = req.validationErrors();
 
-    let errors = req.validationErrors();
-
-    if(errors){
-        res.render('add_article', {
-            title: 'Add Article',
-            errors: errors
-        });
-    } else {
-        // This is using our models
-        let article = new Article();
-        // This is using body parser
-        article.title = req.body.title 
-        article.author = req.body.author 
-        article.body = req.body.body 
-
-        article.save(function(err){
-            if(err){
-                console.log(err);
-                return;
-            } else {
-                req.flash('success', 'Article Added');
-                res.redirect('/');
-            }
-        });        
-    }
-});
-
-// Update Submit  POST route
-// Catches submitted items from edit_article
-
-router.post('/edit/:id', function(req, res){
-    let article = {};
-    // This is using body parser
-    article.title = req.body.title 
-    article.author = req.body.author 
-    article.body = req.body.body 
-
-    let query = {_id:req.params.id}
-
-    Article.update(query, article, function(err){
-        if(err){
-            console.log(err);
-            return;
-        } else {
-            req.flash('success', 'Article Updated');
-            res.redirect('/');
-        }
+  if(errors){
+    res.render('add_article', {
+      title:'Add Article',
+      errors:errors
     });
+  } else {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.user._id;
+    article.body = req.body.body;
+
+    article.save(function(err){
+      if(err){
+        console.log(err);
+        return;
+      } else {
+        req.flash('success','Article Added');
+        res.redirect('/');
+      }
+    });
+  }
 });
 
+// Load Edit Form
+router.get('/edit/:id', ensureAuthenticated, function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    if(article.author != req.user._id){
+      req.flash('danger', 'Not Authorized');
+      res.redirect('/');
+    }
+    res.render('edit_article', {
+      title:'Edit Article',
+      article:article
+    });
+  });
+});
+
+// Update Submit POST Route
+router.post('/edit/:id', function(req, res){
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      req.flash('success', 'Article Updated');
+      res.redirect('/');
+    }
+  });
+});
+
+// Delete Article
 router.delete('/:id', function(req, res){
-    let query = {_id:req.params.id}
-    Article.remove(query, function(err){
+  if(!req.user._id){
+    res.status(500).send();
+  }
+
+  let query = {_id:req.params.id}
+
+  Article.findById(req.params.id, function(err, article){
+    if(article.author != req.user._id){
+      res.status(500).send();
+    } else {
+      Article.remove(query, function(err){
         if(err){
-            console.log(err);
+          console.log(err);
         }
         res.send('Success');
-    });
+      });
+    }
+  });
 });
 
 // Get Single Article
-// :id is a placeholder and could be anything
 router.get('/:id', function(req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('article', {
-            article: article
-        });
+  Article.findById(req.params.id, function(err, article){
+    User.findById(article.author, function(err, user){
+      res.render('article', {
+        article:article,
+        author: user.name
+      });
     });
+  });
 });
+
+// Access Control
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  } else {
+    req.flash('danger', 'Please login');
+    res.redirect('/users/login');
+  }
+}
 
 module.exports = router;
