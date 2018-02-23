34785881866a26ab3125f456b34b6177a25c5d9a
Rob Waller <r.t.waller@gmail.com>
2018-02-23 19:21:58 +0000

    production changes

--- app.js
+++ app.js
@@ -109,6 +109,6 @@ app.use('/articles', articles);
 app.use('/users', users);
 
 // Start Server
-app.listen(3000, function(){
-    console.log('Server started on port 3000');
+app.listen(80, function(){
+    console.log('Server started on port 80');
 });
